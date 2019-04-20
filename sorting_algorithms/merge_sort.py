3cccc30586fdce7f82f37eae8be369cf7b8e6f94
Josh Sisto <joshsisto@gmail.com>
2019-04-20 15:11:28 -0700

    Merge sort is able to handle the largest numbers

---
+++ sorting_algorithms/merge_sort.py
@@ -0,0 +1,52 @@
+# https://interactivepython.org/runestone/static/pythonds/SortSearch/TheMergeSort.html?highlight=merge%20sort
+
+import time
+
+
+def timeit(method: object) -> object:
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print('%r %2.2f sec \n %r, %r' % (method.__name__, te - ts, args, kw))
+
+        return result
+
+    return timed
+
+
+# @timeit
+def merge_sort(alist):
+    # print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    # print("Merging ", alist)
+    return alist
--- sorting_algorithms/random_numbers.py
+++ sorting_algorithms/random_numbers.py
@@ -1,15 +1,17 @@
 import random
 from insertion_sort import *
 from selection_sort import *
+from merge_sort import *
 
 
-my_randoms = random.sample(range(1, 10000000000), 20000)
+my_randoms = random.sample(range(1, 10000000000), 1000000)
 
 print(my_randoms)
 
 
-print(insertion_sort(my_randoms))
-selection_sort(my_randoms)
-selection_sort_2(my_randoms)
+# print(insertion_sort(my_randoms))
+# selection_sort(my_randoms)
+# selection_sort_2(my_randoms)
+print(merge_sort(my_randoms))
 
 
